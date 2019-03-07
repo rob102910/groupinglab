3ad02e039e704b074edb36cdd504cfaf160719b8
rob102910 <47224682+rob102910@users.noreply.github.com>
2019-03-07 15:33:26 -0500

    Update graph.js

--- graph.js
+++ graph.js
@@ -43,7 +43,7 @@ var yScale = d3.scaleLinear()
                 .domain([0,100])
                 .range([0,height]);
   
-var colors = d3.scaleOrdinal.range(["#1f77b4","#ff7f0e","#2ca02c","#d62728","#9467bd","#8c564b","#e377c2","#7f7f7f","#bcbd22","#17becf"]);
+const colors = d3.scaleOrdinal.range(["#1f77b4","#ff7f0e","#2ca02c","#d62728","#9467bd","#8c564b","#e377c2","#7f7f7f","#bcbd22","#17becf"]);
 
                
 //plot land
